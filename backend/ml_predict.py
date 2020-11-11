88ddeaa1f4db0875911927865787f55721f1df6d
David Franco <david.franco@ehu.eus>
2020-11-11 17:21:06 +0000

    comment unnecessary imports

--- backend/ml_predict.py
+++ backend/ml_predict.py
@@ -1,17 +1,16 @@
 import pandas as pd
-from pandas import DataFrame
+#from pandas import DataFrame
 import numpy as np
-import pylab
-import scipy.stats as stats
+#import pylab
+#import scipy.stats as stats
 import matplotlib
 import matplotlib.pyplot as plt
-from pandas.plotting import scatter_matrix
-import random
-from sklearn import datasets, linear_model, tree, cluster, metrics, decomposition
-from sklearn.metrics import roc_curve, auc
-from sklearn.model_selection import train_test_split, cross_val_score, KFold
+#from pandas.plotting import scatter_matrix
+#import random
+#from sklearn import datasets, linear_model, tree, cluster, metrics, decomposition
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
-import pydotplus
+#import pydotplus
 from sklearn.datasets import load_digits
 from sklearn.preprocessing import scale
 import pickle
@@ -168,49 +167,6 @@ class machine_learning:
             #print_cluster(images_test, y_pred, i)
         #plt.show()
 
-        '''
-        # Evaluate the Model
-        print('Adjusted rand score: {0:2}'.format(metrics.adjusted_rand_score(y_test, y_pred)))
-        print('Confussion Matrix:\n',metrics.confusion_matrix(y_test, y_pred))
-        '''
-        
-        '''
-        # Reduce to 2 components
-        pca = decomposition.PCA(n_components=2).fit(X_train)
-        reduced_X_train = pca.transform(X_train)
-        # Step size of the mesh. 
-        h = .01     
-        # point in the mesh [x_min, x_max]x[y_min, y_max].
-        x_min, x_max = reduced_X_train[:, 0].min() + 1, reduced_X_train[:, 0].max() - 1
-        y_min, y_max = reduced_X_train[:, 1].min() + 1, reduced_X_train[:, 1].max() - 1
-        xx, yy = np.meshgrid(np.arange(x_min, x_max, h), 
-            np.arange(y_min, y_max, h))
-        kmeans = cluster.KMeans(init='k-means++', n_clusters=n_digits, n_init=10)
-        kmeans.fit(reduced_X_train)
-        Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
-        '''
-
-        '''
-        # Put the result into a color plot
-        Z = Z.reshape(xx.shape)
-        plt.clf()
-        plt.figure(1)
-        plt.clf()
-        plt.imshow(Z, interpolation='nearest', extent=(xx.min(), xx.max(), yy.min(), yy.max()), 
-            cmap=plt.cm.Paired, aspect='auto', origin='lower')
-        plt.plot(reduced_X_train[:, 0], reduced_X_train[:, 1], 'k.', 
-            markersize=2)
-        # Plot the centroids as a white X
-        centroids = kmeans.cluster_centers_
-        plt.scatter(centroids[:, 0], centroids[:, 1],marker='.', 
-            s=169, linewidths=3, color='w', zorder=10)
-        plt.title('K-means clustering on the digits dataset (PCA reduced data)\nCentroids are marked with white dots')
-        plt.xlim(x_min, x_max)
-        plt.ylim(y_min, y_max)
-        plt.xticks(())
-        plt.yticks(())
-        plt.show()
-        '''
 #machine_learning.clustering()
 #url_classifier="https://archive.ics.uci.edu/ml/machine-learning-databases/undocumented/connectionist-bench/sonar/sonar.all-data"
 #ml = machine_learning()
