08fce0de269c1f6459e982cb7ac29eaf859312af
David Franco <david.franco@ehu.eus>
2020-11-13 13:08:57 +0000

    create de models dir if it does not exist

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,6 @@
 from flask import render_template
 import connexion
+import sys, os
 
 # Create the application instance
 app = connexion.FlaskApp(__name__, specification_dir='./')
@@ -14,6 +15,14 @@ def home():
     # Looks at ./templates and don't know how to change
     return render_template('home.html')
 
+# Check models dir
+def check_models_dir():
+    model_dir = "models"
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+        print("INFO: Directory "+model_dir+" created.")
+
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
+    check_models_dir()
     app.run(host='0.0.0.0', port=5000, debug=True)
