c0e6540fa5b146cb20701f94b22a48410b531633
David Franco <david.franco@ehu.eus>
2021-03-02 09:45:30 +0000

    added stem for load graph

--- mon/get_plots2.py
+++ mon/get_plots2.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import sys, getopt, os
 import pickle
+from scipy.interpolate import splrep, splev
+import numpy as np
 
 def get_opts():
     global test_name
@@ -50,14 +52,20 @@ def split_hosts(host_data, sample_num, host_num):
         hosts_data.append(h)
     return hosts_data
 
-def plot_graph(x,y,label,init,end,xlabel,ylabel,title,filename,plot_dir,br=False):
+def plot_graph(x,y,label,init,end,xlabel,ylabel,title,filename,plot_dir,stem=False):
     #  Check if plot dir exists and create it
     if not os.path.exists(plot_dir):
         os.makedirs(plot_dir)
     # Plot figure and save
     plt.figure()
     for i in range(0,len(y)):
-        plt.plot(x[init:end],y[i][init:end],label=label[i])
+        #smooth = splrep(x[init:end],y[i][init:end],s=3)
+        #y_smooth = splev(x[init:end],smooth)
+        #plt.plot(x[init:end],y_smooth,label=label[i])
+        if(stem):
+            plt.stem(x[init:end],y[i][init:end],label=label[i])
+        else:
+            plt.plot(x[init:end],y[i][init:end],label=label[i])
     plt.legend()
     axes = plt.gca()
     axes.set_xlim([x[init],x[end-1]])
@@ -110,6 +118,28 @@ def get_data(hosts_data):
         x.append(xx)
     return y_load, y_ram, y_br, x
 
+def zero_to_nan(values):
+    """Replace every 0 with 'nan' and return a copy."""
+    return [float('nan') if x==0 else x for x in values]
+
+def interpolate_gaps(values, limit=None):
+    """
+    Fill gaps using linear interpolation, optionally only fill gaps up to a
+    size of `limit`.
+    """
+    values = np.asarray(values)
+    i = np.arange(values.size)
+    valid = np.isfinite(values)
+    filled = np.interp(i, i[valid], values[valid])
+
+    if limit is not None:
+        invalid = ~valid
+        for n in range(1, limit+1):
+            invalid[:-n] &= invalid[n:]
+        filled[invalid] = np.nan
+
+    return filled
+
 def main():
     get_opts()
     hosts_data = load_from_file(test_name)
@@ -127,7 +157,7 @@ def main():
         label_load_ram.append("host_"+str(i))
         label_br.append("host_"+str(i)+" RX")
         label_br.append("host_"+str(i)+" TX")
-    plot_graph(x,y,label_load_ram,0,len(x),"Time(s)","Load (%)","Load Percentage",test_name+"-load_pct.png",plot_dir)
+    plot_graph(x,y,label_load_ram,0,len(x),"Time(s)","Load (%)","Load Percentage",test_name+"-load_pct.png",plot_dir, stem=True)
     # Plot used ram pct
     y = used_ram_pct
     plot_graph(x,y,label_load_ram,0,len(x),"Time(s)","Used RAM (%)","Used RAM Percentage",test_name+"-used_ram_pct.png",plot_dir)
@@ -135,6 +165,11 @@ def main():
     y=list()
     for br in br_mbps:
         tx,rx = split_xy(br) # returns first the second position, which is tx
+        #print(rx)
+        #rx = zero_to_nan(rx) # convert zeros to 'nan' (not plotable)
+        #rx = interpolate_gaps(rx)
+        #tx = zero_to_nan(tx)
+        #print(rx)
         y.append(rx)
         y.append(tx)
     plot_graph(x,y,label_br,0,len(x),"Time(s)","Bitrate (Mbps)","RX/TX Bitrate",test_name+"-br_txrx_mbps.png",plot_dir)
